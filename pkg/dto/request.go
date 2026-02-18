be2ffd48ece3b7063c80017833486f758e58dea9
Shivansh Anand <shivansh586@gmail.com>
2026-02-18 14:38:11 +0530

    minor changes

--- pkg/dto/request.go
+++ pkg/dto/request.go
@@ -1,21 +1,5 @@
 package dto
 
-// Example request DTOs - replace with your actual request structures
-
-// CreateDeploymentRequest represents a request to create a deployment
-type CreateDeploymentRequest struct {
-	Name      string `json:"name" validate:"required,min=3,max=50"`
-	Namespace string `json:"namespace" validate:"required,min=1,max=63"`
-	Image     string `json:"image" validate:"required"`
-	Replicas  int    `json:"replicas" validate:"required,gte=1,lte=100"`
-}
-
-// UpdateDeploymentRequest represents a request to update a deployment
-type UpdateDeploymentRequest struct {
-	Replicas *int    `json:"replicas,omitempty" validate:"omitempty,gte=1,lte=100"`
-	Image    *string `json:"image,omitempty" validate:"omitempty"`
-}
-
 // CreateDeploymentRequestWithMetadata represents a request to create a deployment with metadata
 type CreateDeploymentRequestWithMetadata struct {
 	Name      string             `json:"name" validate:"required,min=3,max=50"`
